3d96fb2b6f5e66d35e77568fad559978c2580af0
WolfLOL1 <62614669+WolfLOL1@users.noreply.github.com>
2021-11-15 10:40:36 +0300

    Add files via upload

---
+++ menu/footer.php
@@ -0,0 +1,53 @@
+</div>
+<div id="footer" class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6 col-sm-4 col-md-3">
+                <div class="address-holder">
+                    <div class="phone"><i class="fas fa-phone"></i>discord.gg/okyanus</div>
+                    <div class="email"><i class="fas fa-envelope"></i> iletisim@okyanusot.com</div>
+                    <div class="address">  
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-2 col-md-2">
+                <div class="footer-menu-holder">
+                    <h4>Adresler</h4>
+                    <ul class="footer-menu">
+                        <li><a href="genelkurallar.php">Kurallar</a></li>
+                        <li><a href="sunucu.php">Sunucular</a></li>
+                        <li><a href="connect.php">İletişim</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xs-6 col-sm-2 col-md-3">
+                <div class="footer-menu-holder">
+                    <h4>Uzantılar</h4>
+                    <ul class="footer-menu">
+                        <li><a href="https://discord.gg/okyanus">Okyanus Discord</a></li>
+                        <li><a href="https://steamcommunity.com/groups/okyanusot">Okyanus Steam</a></li>
+                        <li><a href="https://www.gametracker.com/server_info/185.193.165.142:27015/">Okyanus Gametracker</a></li>
+                        <li><a href="steam://connect/185.193.165.142:27015">Okyanus Sunucu</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-1 col-md-1">
+                <div class="social-menu-holder">
+                    <ul class="social-menu">
+                        <li><a href="https://steamcommunity.com/groups/okyanusot"><i class="fab fa-steam"></i></a></li>
+                        <li><a href="https://www.youtube.com/channel/UCMdGV7SRMiEkUwfcTSHDwHw"><i class="fab fa-youtube"></i></a></li>
+                        <li><a href="https://discord.gg/okyanus"><i class="fab fa-discord"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap-slider.min.js"></script>
+<script src="js/slick.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ menu/header2.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no">
+<title>Okyanus Oyuncu Topluluğu | CS:GO Jailbreak Sunucusu</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.min.css">
+<link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
+<link rel="stylesheet" type="text/css" href="css/slick.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/custom.css">
+<link rel="shortcut icon" href="images/okyanuslogo.ico"/>
+<meta name="description" content="2020 yılından beri Okyanus Jailbreak adı altında şan ve şöhret peşinde koşmadan yolumuza devam ediyoruz. Amacımız sıralamaya girmek değil, aynı kafa yapısına sahip olan insanları bir araya getirerek birlikte eğlenmemizdir.">		
+<meta property="og:description" content="2020 yılından beri Okyanus Jailbreak adı altında şan ve şöhret peşinde koşmadan yolumuza devam ediyoruz. Amacımız sıralamaya girmek değil, aynı kafa yapısına sahip olan insanları bir araya getirerek birlikte eğlenmemizdir." />
+<meta property="og:image" content="https://cdn.discordapp.com/attachments/873941953962725396/873942039975297097/Mavi_Kapal.png" />
+<meta property="og:title" content="Okyanus Oyuncu Topluluğu"/> 
+<meta property="og:site_name" content="Okyanus Oyuncu Topluluğu"/> 
+<meta property="og:type" content="Okyanus Oyuncu Topluluğu"/> 
+<meta property="og:url" content="https://okyanusot.com"/> 
+<meta name="revisit-after" content="7 days">
+<meta name="rating" content="General">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="robots" content="ALL">
+<meta name="author" content="Okyanusot.com">
+<meta name="keywords" content="okyanus jailbreak,okyanus jail,okyanus jb,okyanus csgo,csgo plugin,helheim jailbreak,mekan jailbreak,sonjeton jailbreak,marthex,merseh,efedeniz,Suskanka jailbreak,imparator jailbreak,kıraathane jailbreak,wose jailbreak,cat jailbreak,pcsyo jailbreak,anormal jailbreak,peqa jailbreak,eightborn jailbreak,karakurşun jailbreak,CS:GO,cs,csgo,cs go,CS:GO pluginleri, CS:GO Plugin, CS GO pluginleri, CS GO Plugin, CS GO Plugin satın al, CS:GO !ws !knife pluginleri, minecraft, minecraft plugin, minecraft plugin paketi, minecraft haritaları, minecraft sunucu sitesi, minecraft site script">
+<meta name="copyright" content="2021">
+<meta name="generator" content="Okyanusot.com">
+<meta name="classification" content="Turkey">
+<meta name="distribution" content="Global">
+<meta name="Publisher" content="https://okyanusot.com/">
+<meta name="Dmoz" content="okyanus jailbreak,okyanus jail,okyanus jb,okyanus csgo,csgo plugin,helheim jailbreak,mekan jailbreak,sonjeton jailbreak,marthex,merseh,efedeniz,Suskanka jailbreak,imparator jailbreak,kıraathane jailbreak,wose jailbreak,cat jailbreak,pcsyo jailbreak,anormal jailbreak,peqa jailbreak,eightborn jailbreak,karakurşun jailbreak,CS:GO,cs,csgo,cs go,CS:GO pluginleri, CS:GO Plugin, CS GO pluginleri, CS GO Plugin, CS GO Plugin satın al, CS:GO !ws !knife pluginleri, minecraft, minecraft plugin, minecraft plugin paketi, minecraft haritaları, minecraft sunucu sitesi, minecraft site script">
+<meta name="Altavista" content="okyanus jailbreak,okyanus jail,okyanus jb,okyanus csgo,csgo plugin,helheim jailbreak,mekan jailbreak,sonjeton jailbreak,marthex,merseh,efedeniz,Suskanka jailbreak,imparator jailbreak,kıraathane jailbreak,wose jailbreak,cat jailbreak,pcsyo jailbreak,anormal jailbreak,peqa jailbreak,eightborn jailbreak,karakurşun jailbreak,CS:GO,cs,csgo,cs go,CS:GO pluginleri, CS:GO Plugin, CS GO pluginleri, CS GO Plugin, CS GO Plugin satın al, CS:GO !ws !knife pluginleri, minecraft, minecraft plugin, minecraft plugin paketi, minecraft haritaları, minecraft sunucu sitesi, minecraft site script">
+<meta name="GoogleRobot" content="index,follow">	
+<meta name="Scooter" content="Okyanusot.com">
+</head>
+
+<body>
+
+<div id="header-holder" class="inner-header serverspage-header">
+    <nav id="nav" class="navbar navbar-default navbar-full">
+        <div class="container-fluid">
+            <div class="container container-nav">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="navbar-header">
+                            <button aria-expanded="false" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                           <a class="logo-holder" href="index.php">
+                                <div class="logo" style="width:200px;height:50px"></div>
+                            </a>
+                        </div>
+                        <div style="height: 1px;" role="main" aria-expanded="false" class="navbar-collapse collapse" id="bs">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="index">Anasayfa</a></li>
+                                <li class="dropdown unity-menu">
+                                    <a href="#pricing">Sunucular <i class="fas fa-caret-down"></i></a>
+                                    <ul class="dropdown-menu dropdown-unity">
+                                        <li>
+                                            <a class="unity-link" href="jailbreak">
+                                                <div class="unity-box">
+                                                    <div class="unity-icon">
+                                                        <img src="images/csgo.png" alt="">
+                                                    </div>
+                                                    <div class="unity-title">
+                                                        Jailbreak Sunucumuz
+                                                    </div>
+                                                    <div class="unity-details">
+                                                        Jailbreak sunucumuza katılarak sende isyan atabilirsin.
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </li>
+
+ 
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#">Kurallar <i class="fas fa-caret-down"></i></a>
+                                    <ul class="dropdown-menu">
+                                      <li><a href="genelkurallar">Genel Kurallar</a></li>
+                                      <li><a href="kurallar">Discord Kuralları</a></li>
+									  <li><a href="ceza">Ceza Süreleri</a></li>
+                                      
+                                    </ul>
+                                </li>
+								<li><a href="basvuru">Başvuru</a></li>
+                                <li class="support-button-holder support-dropdown">
+                                    <a class="support-button" href="#">Destek</a>
+                                    <ul class="dropdown-menu">
+                                      <li><a href="https://discord.gg/okyanus"><i class="fas fa-phone"></i>Discord</a></li>  
+                                    </ul>
+                                </li>
+                               
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
